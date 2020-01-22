043583a2f6784f9909892bdb386b6126fc0c69f7
Subong-Noah-Kim <soobong1217@gmail.com>
2020-01-23 00:03:24 +0900

    https://www.hackerrank.com/challenges/weather-observation-station-2/problem?h_r=next-challenge&h_v=zen

--- SQL/hackerrank/SQL_Aggregation_WeatherObservationStation2.sql
+++ SQL/hackerrank/SQL_Aggregation_WeatherObservationStation2.sql
@@ -3,4 +3,4 @@ Enter your query here.
 Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
 */
 
-SELECT ROUND(SUM(LAT_N),2), ROUND(SUM(LONG_W),2) FROM STATION;
+SELECT ROUND(SUM(LAT_N),2), ROUND(SUM(LONG_W),2) FROM STATION ;
